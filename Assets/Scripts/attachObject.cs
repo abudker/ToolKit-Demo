7deb2ccc6f535af3efceb93c14e03db84d0451de
abudker <abudker@gmail.com>
2016-05-29 16:55:23 -0700

    Initial Commit

---
+++ Assets/Scripts/attachObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attachObject : MonoBehaviour {
+
+
+	void Awake () {
+	
+	}
+	
+	void FixedUpdate () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            gameObject.AddComponent<FixedJoint>();
+            var rb = other.gameObject.GetComponent<Rigidbody>();
+            gameObject.GetComponent<FixedJoint>().connectedBody = rb;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        var rb = other.gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null && gameObject.GetComponent<FixedJoint>().connectedBody == rb)
+        {   
+           Destroy(gameObject.GetComponent<FixedJoint>());
+        }
+        
+    }
+}
